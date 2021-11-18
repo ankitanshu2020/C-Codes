1671c2a324ef9dccd5d5ea4716ef30e54ee97319
Ankitanshu <ankitanshu9006@gmail.com>
2021-11-18 22:46:17 +0530

    Ak

--- Ch_8_Pr_2.c
+++ Ch_8_Pr_2.c
@@ -11,14 +11,17 @@ int main()
     printf("Enter the value of first string : \n");   
     scanf("%s",st1);
     printf("Enter the value of second string character-by-character : \n");
-    while(c!='\0');
+    
+    while(c!='\n');
     {
         fflush(stdin);
         scanf("%c",&c);
         st2[i]=c;
         i++;
     }
+
     st2[i-1]='\0';
+    
     printf("The value of st1 is %s\n",st1);
     printf("The value of st2 is %s\n",st2);
     printf("strcmp for these strings returns %d",strcmp(st1,st2));
